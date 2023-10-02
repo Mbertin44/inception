0fb969f24008a1a7bb61a3d362b0d18e3173b526
Morgan Bertin <mbertin@c3r7p5.42quebec.com>
2023-10-02 17:50:35 -0400

    docker compose fonctionne. Faire des tests sur une vm

---
+++ srcs/requirements/mariadb/conf/initmysqld.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+mysqld
+# Je laisse le temps a mysql de ce lancer 
+# Je crée la base de donnée
+mysql --wait -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;" 
+# Je créé un utilisateur
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+# J'accorde les privilèges à l'utilisateur
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';" 
+# Je modifie le mot de passe root
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# #e recharge les privilèges d'utilisateur pour appliquer immédiatement les modifications
+mysql -e "FLUSH PRIVILEGES;"
+# J'arrête le service MySQL
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+# J'attend que le serveur MySQL soit prêt
+mysql --wait
+# J'exécute le serveur MySQL en arrière-plan (pas sur que ca soit nécéssaire)
+exec mysqld_safe 
--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -6,7 +6,7 @@ sleep 30
 
 if [ ! -f "$wp_config_file" ]; then
     # --------------------- 1er étape : Configuration de la base de données SQL pour WordPress ---------------------
-    echo "Configuration de SQL"
+    echo "Configuration de php"
 	wp --allow-root config create \
         --dbname="$SQL_DATABASE" \
         --dbuser="$SQL_USER" \
@@ -17,19 +17,19 @@ if [ ! -f "$wp_config_file" ]; then
     echo "Création de la page"
     wp --allow-root core install \
         --url='https://mbertin.42.fr' \
-        --title="$TITLE" \
-        --admin_user="$ADMIN" \
-        --admin_password="$ADMIN_PASSWORD" \
-        --admin_email="$ADMIN_EMAIL" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN" \
+        --admin_password="$WP_ADMIN_PASSWORD" \
+        --admin_email="$WP_ADMIN_EMAIL" \
         --path='/var/www/wordpress/'
 
     # --------------------- 3eme étape : Ajout d'un utilisateur ---------------------
     echo "Création d'un nouvelle utilisateur"
     wp --allow-root user create \
-        "$USER" \
-        "$USER_EMAIL" \
+        "$WP_USER" \
+        "$WP_USER_EMAIL" \
         --role=author \
-        --user_pass="$USER_PASSWORD" \
+        --user_pass="$WP_USER_PASSWORD" \
         --path='/var/www/wordpress/'
 
     if [ ! -d "$php_file" ]; then

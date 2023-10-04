6538a88f45226d01b9a4b553ee9a5b8e793f5330
Mbertin44 <morganbertin44@gmail.com>
2023-10-03 23:24:37 -0400

    Correction de script et du dockerfile du container wordpress

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,28 +1,24 @@
 #!/bin/sh
 wp_config_file="/var/www/wordpress/wp-config.php"
-php_file="/run/php"
-ISDONE="/var/www/wordpress/done.txt"
 
 echo "Debut du sleep"
 sleep 30
 echo "Fin du sleep"
 
+if [ -f "$wp_config_file" ]; then
+    echo "Suppression du fichier wp-config.php"
+    rm -rf $wp_config_file
+fi
 
-
-
-# if [ ! -f "$ISDONE" ]; then
-# 	touch "$ISDONE"
+if [ ! -f "$wp_config_file" ]; then
     # --------------------- 1er étape : Configuration de la base de données SQL pour WordPress ---------------------
     echo "Configuration de php"
-    sed -i "s/username_here/$MYSQL_USER/g" /var/www/wordpress/wp-config.php
-    sed -i "s/password_here/$MYSQL_PASSWORD/g" /var/www/wordpress/wp-config.php
-    sed -i "s/localhost/$MYSQL_HOSTNAME/g" /var/www/wordpress/wp-config.php
-    sed -i "s/database_name_here/$MYSQL_DATABASE/g" /var/www/wordpress/wp-config.php
     # wp --allow-root config create \
-    #     --dbname="$SQL_DATABASE" \
-    #     --dbuser="$SQL_USER" \
-    #     --dbpass="$SQL_PASSWORD" \
-    #     --dbhost="mariadb:3306" --path='/var/www/wordpress'
+    /usr/local/bin/wp-cli.phar --allow-root config create \
+        --dbname="$SQL_DATABASE" \
+        --dbuser="$SQL_USER" \
+        --dbpass="$SQL_PASSWORD" \
+        --dbhost="mariadb:3306" --path='/var/www/wordpress'
 
     # --------------------- 2eme étape : Création de la page ---------------------
     echo "Création de la page"
@@ -42,39 +38,8 @@ echo "Fin du sleep"
         --role=author \
         --user_pass="$WP_USER_PASSWORD" \
         --path='/var/www/wordpress/'
-# fi
+fi
 
 echo "Execution de php-fpm"
 /usr/sbin/php-fpm7.3 -F 
 
-# if [ ! -f "$wp_config_file" ]; then
-#     # --------------------- 1er étape : Configuration de la base de données SQL pour WordPress ---------------------
-#     echo "Configuration de php"
-# 	wp --allow-root config create \
-#         --dbname="$SQL_DATABASE" \
-#         --dbuser="$SQL_USER" \
-#         --dbpass="$SQL_PASSWORD" \
-#         --dbhost="mariadb:3306" --path='/var/www/wordpress'
-
-#     # --------------------- 2eme étape : Création de la page ---------------------
-#     echo "Création de la page"
-#     wp --allow-root core install \
-#         --url='https://mbertin.42.fr' \
-#         --title="$WP_TITLE" \
-#         --admin_user="$WP_ADMIN" \
-#         --admin_password="$WP_ADMIN_PASSWORD" \
-#         --admin_email="$WP_ADMIN_EMAIL" \
-#         --path='/var/www/wordpress/'
-
-#     # --------------------- 3eme étape : Ajout d'un utilisateur ---------------------
-#     echo "Création d'un nouvelle utilisateur"
-#     wp --allow-root user create \
-#         "$WP_USER" \
-#         "$WP_USER_EMAIL" \
-#         --role=author \
-#         --user_pass="$WP_USER_PASSWORD" \
-#         --path='/var/www/wordpress/'
-# fi
-
-# echo "Execution de php-fpm"
-# /usr/sbin/php-fpm7.3 -F 
